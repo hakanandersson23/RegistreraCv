2c22a491c757d836cde50b9940a10cd0449fa302
håkan <hakan.andersson23@gmail.com>
2019-10-23 15:59:25 +0200

    dsa

--- InteraktivaWebbaplikationerGr1/MainWindow.xaml.cs
+++ InteraktivaWebbaplikationerGr1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace InteraktivaWebbaplikationerGr1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int hakan = 30;
+        }
     }
 }
